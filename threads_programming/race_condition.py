9d66252fa6d178fc754ae6b217b37a40ece6b4b0
MNoori-coder <noorimohammad730@gmail.com>
2025-08-06 15:54:59 +0330

    Race condition added.

---
+++ threads_programming/race_condition.py
@@ -0,0 +1,49 @@
+import threading
+
+available_seats = 2
+
+class Movie:
+    def __init__(self):
+        self.lock = threading.Lock()
+
+    def booking(self, number_of_seats):
+        global available_seats
+
+        with self.lock:
+            try:
+                if number_of_seats <= available_seats:
+                    print(f'{number_of_seats} seats booked successfully for {threading.current_thread().name}\n')
+                    available_seats -= number_of_seats
+                elif number_of_seats > available_seats:
+                    print(f'{threading.current_thread().name}, number of seats that you want is greater than available seats')
+                else:
+                    print('All seats are sold out!')
+            except Exception as e:
+                print(e)
+
+        # self.lock.acquire()
+        # try:
+        #     if number_of_seats <= available_seats:
+        #         print(f'{number_of_seats} seats booked successfully for {threading.current_thread().name}\n')
+        #         available_seats -= number_of_seats
+        #     elif number_of_seats > available_seats:
+        #         print(f'{threading.current_thread().name}, number of seats that you want is greater than available seats')
+        #     else:
+        #         print('All seats are sold out!')
+        # except Exception as e:
+        #     print(e)
+        # finally:
+        #     self.lock.release()
+
+
+movie = Movie()
+
+threads = [
+    threading.Thread(target=movie.booking, args=(2,), name='Mohammad'),
+    threading.Thread(target=movie.booking, args=(2,), name='Ali')
+]
+
+[t.start() for t in threads]
+[t.join() for t in threads]
+
+print('Available seats:', available_seats)
